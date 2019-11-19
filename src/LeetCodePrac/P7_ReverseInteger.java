7030686a350754e9e5e44079df4b7e064546bb54
HuangHeng <973157930@qq.com>
2019-11-19 11:08:08 +0800

    P7_整数反转

--- src/LeetCodePrac/P7_ReverseInteger.java
+++ src/LeetCodePrac/P7_ReverseInteger.java
@@ -28,8 +28,9 @@ public class P7_ReverseInteger {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int reverse(int x) {
-            if(x < 0) {
+        //将数字转为字符串后再进行判断
+        /*public int reverse(int x) {
+            /*if(x < 0) {
                 String numStr = Integer.toString(x).substring(1);
 
                 String reversedStr = swap(numStr);
@@ -60,6 +61,27 @@ public class P7_ReverseInteger {
                 reversedStr = str.charAt(i) + reversedStr;
             }
             return reversedStr;
+        }*/
+
+        //数学方法，不断取模，将数反转，并进行判断是否会溢出
+        public int reverse(int x) {
+            int reversed = 0;
+            while(x != 0) {
+                //获得最后一位出栈的数
+                int pop = x % 10;
+                x = x / 10;
+
+                //每一次入栈前，都和int的最大最小值进行比较，判断是否溢出
+                //int类型最大数和最小数开头的数字只能是1或2，
+                // 所以如果有最后一次循环的话，pop的值一定为1或2，
+                // 所以(rev == INT_MAX / 10 && pop > 7)和(rev == INT_MIN / 10 && pop < -8)判断可以省略
+                if(reversed > Integer.MAX_VALUE / 10) return 0;
+                if(reversed < Integer.MIN_VALUE / 10) return 0;
+
+                //反转后进栈
+                reversed = reversed * 10 + pop;
+            }
+            return reversed;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
